ef61b760b8bd03b70a6bc09385a0be12682090f5
Tarpos <86947313+Tarpos@users.noreply.github.com>
2021-07-05 12:05:12 +0530

    See this

---
+++ script.js
@@ -0,0 +1,23 @@
+// Project 1: Your Age in Days
+
+function ageInDays() {
+    var birthYear = prompt("What year were you born in... Good friend?");
+    var ageInDayss = (2021 - birthYear) * 365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old.');
+    h1.setAttribute('id', 'ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset() {
+    document.getElementById('ageInDays').remove();
+}
+
+// Project 2: Cat Generator
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
+}
